42026ec2a4db4b3cbc8b8187c07bb14fdd874cce
Adam Mackintosh <agm1984@gmail.com>
2017-04-22 18:41:25 -0700

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("test");
+var express = require('express');
+var app = express();
+
+// Home Route
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+});
+
+// Love Route
+app.get("/fallinlovewith/:thing", function (req, res) {
+    var thing = req.params.thing;
+    res.render("love.ejs", { thingVar: thing });
+});
+
+// Server config
+config = {};
+config.port = 3000;
+config.host = "localhost";
+
+// Start server
+app.listen(config.port, config.host, function () {
+  console.log("Server is listening on port " + config.port);
+});
